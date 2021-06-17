f2750aea87cdc598748f19f9ca41d3750befbb3a
Dmitrii Cymbal <tsymbal.mitia@gmail.com>
2021-06-17 16:37:18 +0300

    обновление

--- fields/yandexmaps/media/js/yandexmapfield.js
+++ fields/yandexmaps/media/js/yandexmapfield.js
@@ -1,22 +1,20 @@
-jQuery(document).ready(function () {
+document.addEventListener("DOMContentLoaded", function () {
 
     ymaps.load(function () {
-        jQuery('.in-yandex-maps').each(function (i, el) {
-            (new ymaps.SuggestView(jQuery(el).attr('id'))).events.add('select', function (event) {
+        let list = document.querySelectorAll('.in-yandex-maps');
+        for (let i = 0; i < list.length; i++) {
+            (new ymaps.SuggestView(list[i].getAttribute('id'))).events.add('select', function (event) {
                 ymaps.geocode(event.get('item').value).then(function (res) {
                     var id = event.get('target')._panel._anchor.id;
                     var obj = res.geoObjects.get(0),
                         error, hint;
                     var fulladdress = obj.properties.get('metaDataProperty.GeocoderMetaData.Address.formatted');
                     var point = obj.properties.get('metaDataProperty.GeocoderMetaData.InternalToponymInfo.Point');
-
-                    jQuery('#' + id).val(fulladdress + ';' + point.coordinates[1] + ',' + point.coordinates[0]);
-
+                    list[i].value = fulladdress + ';' + point.coordinates[1] + ',' + point.coordinates[0];
                 }, function (e) {
                 })
             });
-        })
+        }
     });
-
 
 });
