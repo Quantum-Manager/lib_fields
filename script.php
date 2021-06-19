3abd06c0e4736f7bbd20c58ccf36452b14a8a1ae
Dmitrii Cymbal <tsymbal.mitia@gmail.com>
2021-06-19 22:17:03 +0300

    обновление

--- script.php
+++ script.php
@@ -1,8 +1,42 @@
-<?php
+<?php defined('_JEXEC') or die;
+
+use Joomla\Filesystem\Folder;
+
 /**
- * @package     ${NAMESPACE}
- * @subpackage
- *
- * @copyright   A copyright
- * @license     A "Slug" license name e.g. GPL2
+ * Class Lib_fieldsInstallerScript
  */
+class Lib_fieldsInstallerScript
+{
+
+
+	public function postflight($type, $parent)
+	{
+		$this->copyMedia($parent->getParent());
+
+		return true;
+	}
+
+
+	protected function copyMedia($installer)
+	{
+		$path      = JPATH_LIBRARIES . '/lib_fields';
+		$folders   = Folder::folders($path);
+		$copyFiles = [];
+
+		foreach ($folders as $folder)
+		{
+			$path_current = $path . '/' . $folder . '/media';
+			if (file_exists($path_current))
+			{
+				$copyFiles[] = [
+					'src'  => $path_current,
+					'dest' => JPATH_ROOT . '/media/lib_fields/' . $folder,
+					'type' => 'folder'
+				];
+			}
+		}
+
+		return $installer->copyFiles($copyFiles, true);
+	}
+
+}
