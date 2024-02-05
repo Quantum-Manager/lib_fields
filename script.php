9ad325a3c810a664efbbdfc261c7760ce6a27a39
dmitriitsymbal <cymbal@delo-design.ru>
2024-02-05 14:27:44 +0300

    fix script

--- script.php
+++ script.php
@@ -1,89 +1,201 @@
-<?php defined('_JEXEC') or die;
+<?php
+/*
+ * @package     RadicalMart Front Package
+ * @subpackage  plg_radicalmart_front
+ * @version     __DEPLOY_VERSION__
+ * @author      RadicalMart Team - radicalmart.ru
+ * @copyright   Copyright (c) 2023 RadicalMart. All rights reserved.
+ * @license     GNU/GPL license: https://www.gnu.org/copyleft/gpl.html
+ * @link        https://radicalmart.ru/
+ */
 
+\defined('_JEXEC') or die;
+
+use Joomla\CMS\Application\AdministratorApplication;
+use Joomla\CMS\Factory;
+use Joomla\CMS\Installer\InstallerAdapter;
+use Joomla\CMS\Installer\InstallerScriptInterface;
+use Joomla\Database\DatabaseDriver;
+use Joomla\DI\Container;
+use Joomla\DI\ServiceProviderInterface;
 use Joomla\Filesystem\Folder;
 use Joomla\Filesystem\Path;
 
-if (class_exists('Lib_fieldsInstallerScript'))
-{
-	return;
-}
-
-/**
- * Class Lib_fieldsInstallerScript
- */
-class Lib_fieldsInstallerScript
-{
-
-
-	public function postflight($type, $parent)
+return new class () implements ServiceProviderInterface {
+	public function register(Container $container)
 	{
+		$container->set(InstallerScriptInterface::class, new class ($container->get(AdministratorApplication::class)) implements InstallerScriptInterface {
+			/**
+			 * The application object
+			 *
+			 * @var  AdministratorApplication
+			 *
+			 * @since  __DEPLOY_VERSION__
+			 */
+			protected AdministratorApplication $app;
 
-		if ($type === 'install' || $type === 'update')
-		{
-			$this->copyMedia($parent->getParent());
-		}
+			/**
+			 * The Database object.
+			 *
+			 * @var   DatabaseDriver
+			 *
+			 * @since  __DEPLOY_VERSION__
+			 */
+			protected DatabaseDriver $db;
 
-		if ($type === 'uninstall')
-		{
-			$this->deleteMedia();
-		}
-
-		return true;
-	}
-
-
-	protected function copyMedia($installer)
-	{
-		$dest    = JPATH_ROOT . '/media/lib_fields';
-		$path    = Path::clean(JPATH_ROOT . '/libraries/lib_fields/fields');
-
-		if(!file_exists($path))
-		{
-			return;
-		}
-
-		$folders = Folder::folders($path);
-
-		$copyFiles = [];
-
-		if (!file_exists($dest))
-		{
-			Folder::create($dest);
-		}
-
-		foreach ($folders as $folder)
-		{
-			$path_current = $path . '/' . $folder . '/media';
-			if (file_exists($path_current))
+			/**
+			 * Constructor.
+			 *
+			 * @param   AdministratorApplication  $app  The application object.
+			 *
+			 * @since __DEPLOY_VERSION__
+			 */
+			public function __construct(AdministratorApplication $app)
 			{
-				$copyFiles[] = [
-					'src'  => $path_current,
-					'dest' => $dest . '/' . strtolower($folder),
-					'type' => 'folder'
-				];
+				$this->app = $app;
+				$this->db  = Factory::getContainer()->get('DatabaseDriver');
 			}
-		}
 
-		return $installer->copyFiles($copyFiles, true);
-	}
-
-
-	protected function deleteMedia()
-	{
-		$dest = JPATH_ROOT . '/media/lib_fields';
-
-		if (file_exists($dest))
-		{
-			try
+			/**
+			 * Function called after the extension is installed.
+			 *
+			 * @param   InstallerAdapter  $adapter  The adapter calling this method
+			 *
+			 * @return  boolean  True on success
+			 *
+			 * @since   __DEPLOY_VERSION__
+			 */
+			public function install(InstallerAdapter $adapter): bool
 			{
-				return Folder::delete($dest);
-			}
-			catch (Exception $e)
-			{
-				return false;
-			}
-		}
+				$this->copyMedia();
 
-		return true;
+				return true;
+			}
+
+			/**
+			 * Function called after the extension is updated.
+			 *
+			 * @param   InstallerAdapter  $adapter  The adapter calling this method
+			 *
+			 * @return  boolean  True on success
+			 *
+			 * @since   __DEPLOY_VERSION__
+			 */
+			public function update(InstallerAdapter $adapter): bool
+			{
+				$this->copyMedia();
+
+				return true;
+			}
+
+			/**
+			 * Function called after the extension is uninstalled.
+			 *
+			 * @param   InstallerAdapter  $adapter  The adapter calling this method
+			 *
+			 * @return  boolean  True on success
+			 *
+			 * @since   __DEPLOY_VERSION__
+			 */
+			public function uninstall(InstallerAdapter $adapter): bool
+			{
+				$this->deleteMedia();
+
+				return true;
+			}
+
+			/**
+			 * Function called before extension installation/update/removal procedure commences.
+			 *
+			 * @param   string            $type     The type of change (install or discover_install, update, uninstall)
+			 * @param   InstallerAdapter  $adapter  The adapter calling this method
+			 *
+			 * @return  boolean  True on success
+			 *
+			 * @since   __DEPLOY_VERSION__
+			 */
+			public function preflight(string $type, InstallerAdapter $adapter): bool
+			{
+				return true;
+			}
+
+			/**
+			 * Function called after extension installation/update/removal procedure commences.
+			 *
+			 * @param   string            $type     The type of change (install or discover_install, update, uninstall)
+			 * @param   InstallerAdapter  $adapter  The adapter calling this method
+			 *
+			 * @return  boolean  True on success
+			 *
+			 * @since   __DEPLOY_VERSION__
+			 */
+			public function postflight(string $type, InstallerAdapter $adapter): bool
+			{
+				return true;
+			}
+
+			protected function copyMedia()
+			{
+				$dest = JPATH_ROOT . '/media/lib_fields';
+				$path = Path::clean(JPATH_ROOT . '/libraries/lib_fields/fields');
+
+				if (!file_exists($path))
+				{
+					return;
+				}
+
+				$folders = Folder::folders($path);
+
+				$copyFiles = [];
+
+				if (!file_exists($dest))
+				{
+					Folder::create($dest);
+				}
+
+				foreach ($folders as $folder)
+				{
+					$path_current = $path . '/' . $folder . '/media';
+					if (file_exists($path_current))
+					{
+						$copyFiles[] = [
+							'src'  => $path_current,
+							'dest' => $dest . '/' . strtolower($folder),
+							'type' => 'folder'
+						];
+					}
+				}
+
+				foreach ($copyFiles as $copy)
+				{
+					if ($copy['type'] == 'folder')
+					{
+						Folder::copy($copy['src'], $copy['dest']);
+					}
+				}
+
+			}
+
+
+			protected function deleteMedia()
+			{
+				$dest = JPATH_ROOT . '/media/lib_fields';
+
+				if (file_exists($dest))
+				{
+					try
+					{
+						return Folder::delete($dest);
+					}
+					catch (Exception $e)
+					{
+						return false;
+					}
+				}
+
+				return true;
+			}
+
+		});
 	}
-}
+};
